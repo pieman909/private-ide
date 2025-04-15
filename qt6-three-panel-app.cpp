89df7d857b0cc743f46977b893245ad845e44acc
Saahith V <66752243+pieman909@users.noreply.github.com>
2025-04-14 19:56:19 -0700

    Update qt6-three-panel-app.cpp

--- qt6-three-panel-app.cpp
+++ qt6-three-panel-app.cpp
@@ -129,6 +129,29 @@ protected:
                 setFormat(match.capturedStart(), match.capturedLength(), rule.format);
             }
         }
+
+        // Multi-line comments
+        setCurrentBlockState(0);
+        
+        int startIndex = 0;
+        if (previousBlockState() != 1)
+            startIndex = text.indexOf("/*");
+        
+        while (startIndex >= 0) {
+            QRegularExpressionMatch match = QRegularExpression("\\*/").match(text, startIndex);
+            int endIndex = match.capturedStart();
+            int commentLength;
+            
+            if (endIndex == -1) {
+                setCurrentBlockState(1);
+                commentLength = text.length() - startIndex;
+            } else {
+                commentLength = endIndex - startIndex + match.capturedLength();
+            }
+            
+            setFormat(startIndex, commentLength, singleLineCommentFormat);
+            startIndex = text.indexOf("/*", startIndex + commentLength);
+        }
     }
 
 private:
